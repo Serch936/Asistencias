51084766770467298f8ad2deac158247c38892e8
EnriquePerez322692 <quiquebolollo19@gmail.com>
2025-09-18 19:33:27 -0600

    Mi primer commit - Programa de asistencias

--- Asitencias.c
+++ Asitencias.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-#include <time.h>
 
-typedef struct AsistenciaAlumno {
-    int id;
-    char nombre[100];
-    char estado; 
-    struct AsistenciaAlumno *siguiente;
-} AsistenciaAlumno;
+typedef struct nodo {
 
-typedef struct DiaClase {
-    char fecha[20];
-    struct DiaClase *siguienteDia;
-    AsistenciaAlumno *listaDeAsistencia; 
-} DiaClase;
+    char nombre[50];
+    int *asistencias;     
+    int numDias;          
+    struct nodo* siguiente;
+    
+} Nodo;
+
+Nodo* crearnodo(char nombre[]) {
+
+    Nodo* nuevo = (Nodo*) malloc(sizeof(Nodo));
+
+    if (nuevo == NULL) {
+
+        printf("Error: no se pudo asignar memoria\n");
+        return;
+
+    }
+
+    strcpy(nuevo->nombre, nombre);
+    nuevo->asistencias = NULL;
+    nuevo->numDias = 0;
+    nuevo->siguiente = NULL;
+
+    return nuevo;
+
+}
 
 
